9118b54e8fea0d8cdeeee255a714d0ec2787554d
Hyleax <normanyaptw@yahoo.com>
2025-09-01 12:23:35 +1000

    added github pages integration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/myeloid-apc-atlas-landing",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
